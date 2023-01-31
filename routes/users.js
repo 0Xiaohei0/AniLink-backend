1b3eefebb5fb1667f42523503580a65311eea6d7
0Xiaohei0 <15852786007ysc@gmail.com>
2023-01-30 21:50:13 -0500

    storing user in local storage

--- routes/users.js
+++ routes/users.js
@@ -6,22 +6,12 @@ var bodyParser = require("body-parser");
 var jsonParser = bodyParser.json();
 
 router.post("/", jsonParser, async (req, res) => {
+  console.log(req.body.uId);
   // find user in database
   let user = await User.findOne({ uId: req.body.uId });
   // if user doesn't exist, create a new user with local storage watchlist
   if (!user) {
-    user = new User({
-      uId: req.body.uId,
-      watchlistData: req.body.watchlistData,
-    });
-    let saveResult = await user.save();
-    console.log(saveResult);
-    let m_response = {
-      message:
-        "User not found in database, uploading watchlist from local storage",
-      watchlistData: req.body.watchlistData,
-    };
-    res.send(m_response);
+    createNewUser(req, res);
   } else {
     // user exists, send the stored watchlist to user
     let m_response = {
@@ -31,5 +21,42 @@ router.post("/", jsonParser, async (req, res) => {
     res.send(m_response);
   }
 });
+
+router.put("/", jsonParser, async (req, res) => {
+  // find user in database
+  console.log(`received put request`);
+  let user = await User.findOne({ uId: req.body.uId });
+  // if user doesn't exist, create a new user with local storage watchlist
+  if (!user) {
+    let m_response = {
+      message: "User not found",
+      watchlistData: [],
+    };
+    res.send(m_response);
+  } else {
+    // user exists, update watchlist
+    user.watchlistData = req.body.watchlistData;
+    user.save();
+    let m_response = {
+      message: "User found, updating watchlist.",
+      watchlistData: user.watchlistData,
+    };
+    res.send(m_response);
+  }
+});
+async function createNewUser(req, res) {
+  user = new User({
+    uId: req.body.uId,
+    watchlistData: req.body.watchlistData,
+  });
+  let saveResult = await user.save();
+  console.log(saveResult);
+  let m_response = {
+    message:
+      "User not found in database, uploading watchlist from local storage",
+    watchlistData: req.body.watchlistData,
+  };
+  res.send(m_response);
+}
 
 module.exports = router;
